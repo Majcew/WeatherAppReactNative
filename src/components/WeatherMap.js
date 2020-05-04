81e4915a105b69d01511d012ec2a0f9f3188c5bc
Szymek <szymonjoszko@gmail.com>
2020-05-04 15:36:40 +0200

    Dodanie layerow do mapy

--- src/components/WeatherMap.js
+++ src/components/WeatherMap.js
@@ -1,3 +1,15 @@
+const api_key = 'c4ffc65c1eda93d335ccc1b45fbab2cc';
+const url_prefix = 'https://tile.openweathermap.org/map/';
+const url_sufix = '/{z}/{x}/{y}.png?appid=';
+const layers = [
+  'https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw',
+  url_prefix + 'temp_new' + url_sufix + api_key,
+  url_prefix + 'pressure_new' + url_sufix + api_key,
+  url_prefix + 'clouds_new' + url_sufix + api_key,
+  url_prefix + 'precipitation_new' + url_sufix + api_key,
+  url_prefix + 'wind_new' + url_sufix + api_key,
+];
+
 const html_map = (lat, lon, zoom, layer) => `
 <!DOCTYPE html>
 <html>
@@ -5,7 +17,7 @@ const html_map = (lat, lon, zoom, layer) => `
 	
 	<title>Quick Start - Leaflet</title>
 	<meta charset="utf-8" />
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
 	
 	<link rel="shortcut icon" type="image/x-icon" href="docs/images/favicon.ico" />
     <link rel="stylesheet" href="https://unpkg.com/leaflet@1.6.0/dist/leaflet.css" integrity="sha512-xwE/Az9zrjBIphAcBb3F6JVqxf46+CDLwfLMHloNu6KEQCAWi6HcDUbeOfBIptF7tcCzusKFjFw2yuvEpDL9wQ==" crossorigin=""/>
@@ -13,17 +25,18 @@ const html_map = (lat, lon, zoom, layer) => `
 	
 </head>
 <body>
-<div id="mapid" style="width: 600px; height: 400px;"></div>
+<div id="mapid" style="width: 100wh; height: 100vh;"></div>
 <script>
 	var mymap = L.map('mapid').setView([${lat}, ${lon}], ${zoom});
-	L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
-		maxZoom: 18,
+	L.tileLayer('${layers[layer]}', {
+        maxZoom: ${zoom},
+        minZoom: ${zoom},
 		attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
 			'<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
 			'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
 		id: 'mapbox/streets-v11',
-		tileSize: 512,
-		zoomOffset: -1
+        tileSize: 512,
+        zoomOffset: -1
 	}).addTo(mymap);
 	L.marker([${lat}, ${lon}]).addTo(mymap)
 		.bindPopup("<b>I'm here</b><br />lat:${lat}<br />lon:${lon}").openPopup();
--- src/screens/Map.js
+++ src/screens/Map.js
@@ -1,12 +1,57 @@
-import React, {Component} from 'react';
-import {View, Text, TouchableOpacity, Image, StyleSheet} from 'react-native';
+import React, {useState} from 'react';
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  Image,
+  StyleSheet,
+  FlatList,
+} from 'react-native';
 import {WebView} from 'react-native-webview';
 import html_map from '../components/WeatherMap';
 
+const DATA = [
+  {
+    id: 0,
+    title: 'Mapa',
+  },
+  {
+    id: 1,
+    title: 'Temperatura',
+  },
+  {
+    id: 2,
+    title: 'Ciśnienie',
+  },
+  {
+    id: 3,
+    title: 'Chmury',
+  },
+  {
+    id: 4,
+    title: 'Opady',
+  },
+  {
+    id: 5,
+    title: 'Wiatr',
+  },
+];
+
 const Map = ({navigation}) => {
-  console.log(navigation);
-  const latitude = 53;
-  const longitude = 0;
+  const [layer = 0, setLayer] = useState();
+  const latitude = 51.5;
+  const longitude = -0.12;
+
+  function Item({item}) {
+    return (
+      <View style={styles.item}>
+        <Text style={styles.title} onPress={() => setLayer(item.id)}>
+          {item.title}
+        </Text>
+      </View>
+    );
+  }
+
   return (
     <>
       <TouchableOpacity
@@ -15,7 +60,16 @@ const Map = ({navigation}) => {
         }}>
         <Image style={styles.drawer} source={require('../img/menu.png')} />
       </TouchableOpacity>
-      <WebView source={{html: html_map(latitude, longitude, 13, 1)}} />
+      <View>
+        <FlatList
+          horizontal
+          data={DATA}
+          renderItem={({item}) => <Item item={item} />}
+          keyExtractor={(item) => item.id}
+        />
+      </View>
+
+      <WebView source={{html: html_map(latitude, longitude, 5, layer)}} />
     </>
   );
 };
@@ -24,6 +78,15 @@ const styles = StyleSheet.create({
   drawer: {
     width: 40,
     height: 40,
+  },
+  item: {
+    backgroundColor: 'dodgerblue',
+    padding: 15,
+    marginVertical: 8,
+    marginHorizontal: 8,
+  },
+  title: {
+    fontSize: 28,
   },
 });
 
