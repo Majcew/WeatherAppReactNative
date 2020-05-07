7136a278b9b054a530e4e4e0d55546ba7e5dc406
Szymek <szymonjoszko@gmail.com>
2020-05-07 20:30:17 +0200

    Dodanie globalnego contextu z koordynatami

--- App.js
+++ App.js
@@ -1,7 +1,13 @@
-import React, {useState} from 'react';
+import React, {useState, useContext} from 'react';
 
 import AppNavigation from './src/navigation/AppNavigation';
+import {CurrentCoords} from './src/context/Coords';
 
 export default function App() {
-  return <AppNavigation />;
+  const [currentCoords, setCurrentCoords] = useState({lat: 0, lon: 0});
+  return (
+    <CurrentCoords.Provider value={[currentCoords, setCurrentCoords]}>
+      <AppNavigation />
+    </CurrentCoords.Provider>
+  );
 }
