e379cb48eed61ac817d4008b2f5f58b017abd6f4
Wiktor Hosumbek <wiktor4444@gmail.com>
2020-05-26 13:36:14 +0200

    update about screen

--- src/screens/About.js
+++ src/screens/About.js
@@ -1,19 +1,72 @@
-import React, {Component} from 'react';
-import {View, Text, TouchableOpacity, Image, StyleSheet} from 'react-native';
+import React, {Component, useCallback} from 'react';
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  Image,
+  StyleSheet,
+  Button,
+  Linking,
+} from 'react-native';
+import Icon from 'react-native-vector-icons/FontAwesome';
+
+const OpenURLText = ({url, children}) => {
+  const handlePress = useCallback(async () => {
+    const supported = await Linking.canOpenURL(url);
+
+    if (supported) {
+      await Linking.openURL(url);
+    } else {
+      Alert.alert(`Don't know how to open this URL: ${url}`);
+    }
+  }, [url]);
+
+  return <Text style={{color:'blue'}} onPress={handlePress}>{children}</Text>;
+};
 
 const About = ({navigation}) => {
   return (
-    <View>
-      <TouchableOpacity
-        onPress={() => {
-          navigation.openDrawer();
+    <View
+      style={{
+        flex: 1,
+        alignContent: 'center',
+        alignItems: 'center',
+        justifyContent: 'center',
+      }}>
+      <View>
+        <View style={{marginLeft: 22}}>
+          <Icon name="umbrella" size={128} color="rgb(88, 86, 214)" />
+        </View>
+        <Text style={{color: 'rgb(88, 86, 214)', fontSize: 28}}>
+          WeatherAPP
+        </Text>
+      </View>
+      <View
+        style={{
+          marginTop: 40,
+          marginHorizontal: 40,
+          marginVertical: 20,
+          alignItems: 'center',
         }}>
-        <Image style={styles.drawer} source={require('../img/menu.png')} />
-      </TouchableOpacity>
-      <Text>
-        Tutaj będzie bardzo piękny tekst na temat naszej apki (nie wazne w tej
-        chwili)
-      </Text>
+        <Text style={{fontWeight: 'bold'}}>O aplikacji:</Text>
+        <Text>
+          Aplikacja pogodynka prezentująca aktualne dane pogodowe wraz z
+          prognoza pogody na następne dni
+        </Text>
+      </View>
+      <View style={{marginVertical: 20}}>
+        <Text style={{fontWeight: 'bold'}}>Twórcy aplikacji:</Text>
+        <Text>Szymon Joszko</Text>
+        <Text>Krzysztof Dragon</Text>
+        <Text>Wiktor Hosumbek</Text>
+        <Text>Szymon Babula</Text>
+      </View>
+      <View>
+        <Text style={{fontWeight: 'bold'}}>Kontakt:</Text>
+        <OpenURLText url={'https://github.com/Majcew/WeatherAppReactNative'}>
+          GitHub
+        </OpenURLText>
+      </View>
     </View>
   );
 };
