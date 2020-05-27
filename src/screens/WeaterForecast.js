b26e50f9cd41a4b5bb4b5e6cab7b30b3e5071176
Majcew <48514316+Majcew@users.noreply.github.com>
2020-05-27 18:13:12 +0200

    duplicate css prop fix

--- src/screens/WeaterForecast.js
+++ src/screens/WeaterForecast.js
@@ -90,7 +90,6 @@ const WeatherForecast = ({navigation}) => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
     flexDirection: 'row',
     justifyContent: 'space-around',
     padding: 10,
