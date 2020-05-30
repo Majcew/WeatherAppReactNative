0fca40eebebdc7b665a575c0094cc41fe01f1de6
Szymek <szymonjoszko@gmail.com>
2020-05-31 01:35:30 +0200

    Dodanie opisu, poprawa layoutu

--- src/screens/UVIndex.js
+++ src/screens/UVIndex.js
@@ -1,26 +1,105 @@
-import React, {useState, useContext} from 'react';
-import {View, Text, TouchableOpacity, Image, StyleSheet} from 'react-native';
+import React, {useState, useContext, useCallback} from 'react';
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  Image,
+  StyleSheet,
+  Linking,
+} from 'react-native';
 import * as API from '../utils/API';
+import {UVIDesc} from '../utils/UVIndexDesc';
 import {CurrentCoords} from '../context/Coords';
+
+const OpenURLText = ({url, children}) => {
+  const handlePress = useCallback(async () => {
+    const supported = await Linking.canOpenURL(url);
+
+    if (supported) {
+      await Linking.openURL(url);
+    } else {
+      Alert.alert(`Don't know how to open this URL: ${url}`);
+    }
+  }, [url]);
+
+  return (
+    <Text
+      style={{color: 'blue', fontSize: 10, marginTop: 10}}
+      onPress={handlePress}>
+      {children}
+    </Text>
+  );
+};
 
 const UVIndex = () => {
   const [currentCoords] = useContext(CurrentCoords);
+  const [risk = 0, setRisk] = useState();
   const [UVi = 0, setUVi] = useState();
+  const allRisks = ['Low', 'Moderate', 'High', 'Very high', 'Extreme'];
+  const allColors = ['green', 'yellow', 'orange', 'red', 'violet'];
 
   const getUVi = async () => {
     try {
       const uvi = await API.getUVIndex(currentCoords.lat, currentCoords.lon);
       if (uvi) {
         await setUVi(uvi.value);
+        await setCurrentRisk(UVi);
       }
     } catch (err) {
       console.log('Error in UVIndex#getUVi: ', err);
     }
   };
+  const setCurrentRisk = (UVi) => {
+    switch (true) {
+      case UVi < 3:
+        setRisk(0);
+        break;
+      case UVi < 6:
+        setRisk(1);
+        break;
+      case UVi < 8:
+        setRisk(2);
+        break;
+      case UVi < 11:
+        setRisk(3);
+        break;
+
+      default:
+        setRisk(4);
+        break;
+    }
+  };
+
+  const setUViStyle = (color) => {
+    return {
+      color: color,
+      marginBottom: 40,
+      fontWeight: 'bold',
+      fontSize: 48,
+    };
+  };
+
+  const setRiskStyle = (color) => {
+    return {
+      color: color,
+      marginBottom: 80,
+      fontSize: 32,
+    };
+  };
   getUVi();
   return (
     <View style={styles.mainView}>
-      <Text>{UVi}</Text>
+      <Text style={setUViStyle(allColors[risk])}>{UVi}</Text>
+      <Text style={styles.riskHeader}>
+        Risk of harm from unprotected Sun exposure, for the average adult:
+      </Text>
+      <Text style={setRiskStyle(allColors[risk])}>{allRisks[risk]}</Text>
+      <View style={styles.infoView}>
+        <Text style={styles.info}>{UVIDesc[risk]}</Text>
+        <OpenURLText url={'https://en.wikipedia.org/wiki/Ultraviolet_index'}>
+          *Data from wikipedia
+        </OpenURLText>
+      </View>
     </View>
   );
 };
@@ -32,21 +111,21 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
     marginHorizontal: 40,
+    position: 'relative',
+    top: -50,
   },
-  icon: {
-    marginLeft: 22,
+  riskHeader: {
+    textAlign: 'center',
+    fontSize: 18,
   },
-  appName: {
-    color: 'rgb(88, 86, 214)',
-    marginBottom: 40,
-    fontSize: 28,
+  infoView: {
+    bottom: -35,
+    display: 'flex',
+    position: 'absolute',
   },
-  textView: {
-    marginVertical: 10,
-    alignItems: 'center',
-  },
-  textHeader: {
-    fontWeight: 'bold',
+  info: {
+    textAlign: 'justify',
+    fontSize: 14,
   },
   drawer: {
     marginRight: 10,
---
+++ src/utils/UVIndexDesc.js
@@ -0,0 +1,27 @@
+export const UVIDesc = [
+  "A UV index reading of 0 to 2 means low danger from the Sun's UV rays for the average person. \
+Wear sunglasses on bright days.If you burn easily, cover up and use broad spectrum SPF 30 + sunscreen. \
+Bright surfaces, such as sand, water, and snow, will increase UV exposure.",
+  'A UV index reading of 3 to 5 means moderate risk of harm from unprotected Sun exposure. \
+Stay in shade near midday when the Sun is strongest.If outdoors, wear Sun protective clothing, \
+a wide - brimmed hat, and UV - blocking sunglasses.Generously apply broad spectrum SPF 30 + sunscreen \
+every 1.5 hours, even on cloudy days, and after swimming or sweating.Bright surfaces, such as sand, water, \
+and snow, will increase UV exposure.',
+  'A UV index reading of 6 to 7 means high risk of harm from unprotected Sun exposure. Protection against \
+skin and eye damage is needed. Reduce time in the Sun between 10 a.m.and 4 p.m.If outdoors, seek shade \
+and wear Sun protective clothing, a wide - brimmed hat, and UV - blocking sunglasses.Generously apply broad \
+spectrum SPF 30 + sunscreen every 1.5 hours, even on cloudy days, and after swimming or sweating.Bright surfaces, \
+ such as sand, water, and snow, will increase UV exposure.',
+  'A UV index reading of 8 to 10 means very high risk of harm from unprotected Sun exposure. \
+Take extra precautions because unprotected skin and eyes will be damaged and can burn quickly. \
+Minimize Sun exposure between 10 a.m.and 4 p.m.If outdoors, seek shade and wear Sun protective \
+clothing, a wide - brimmed hat, and UV - blocking sunglasses.Generously apply broad spectrum SPF \
+30 + sunscreen every 1.5 hours, even on cloudy days, and after swimming or sweating.Bright surfaces, \
+such as sand, water, and snow, will increase UV exposure.',
+  'A UV index reading of 11 or more means extreme risk of harm from unprotected Sun exposure. \
+Take all precautions because unprotected skin and eyes can burn in minutes. Try to avoid Sun \
+exposure between 10 a.m.and 4 p.m.If outdoors, seek shade and wear Sun protective clothing, \
+a wide - brimmed hat, and UV - blocking sunglasses.Generously apply broad spectrum SPF 30 + sunscreen \
+every 1.5 hours, even on cloudy days, and after swimming or sweating.Bright surfaces, such as sand, \
+water, and snow, will increase UV exposure.',
+];
